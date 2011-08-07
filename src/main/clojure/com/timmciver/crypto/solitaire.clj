d17b13d91aee4506e868b7c424b9f228183fa6ac
Tim McIver <tim@timmciver.com>
2011-08-06 23:59:16 -0400

    Have two working functions: move-card-down and triple-cut, passing two tests with three assertions.

--- src/main/clojure/com/timmciver/crypto/solitaire.clj
+++ src/main/clojure/com/timmciver/crypto/solitaire.clj
@@ -1,4 +1,5 @@
-(ns com.timmciver.crypto.solitaire)
+(ns com.timmciver.crypto.solitaire
+  (:use [clojure.contrib.seq :only (positions)]))
 
 (defn solitaire
   [deck]
@@ -12,6 +13,17 @@ from 1 to 54 in any order."
   "Move card card down spaces spaces in deck. If card moves past
 bottom of deck, it cycles back to the top of the deck."
   {:pre [(<= card 54) (<= (count deck) 54)]}
-  (let [[top therest] (split-with #(not= % card) deck)
-        [middle bottom] (split-at spaces (next therest))]
-    (concat top middle (list card) bottom)))
+  (let [old-pos (.indexOf (vec deck) card)
+        new-pos (mod (+ old-pos spaces) (dec (count deck)))
+        [top therest] (split-at new-pos (remove #(= % card) deck))]
+    (concat top (list card) therest)))
+
+(defn triple-cut
+  [deck]
+  "Performs a triple cut around the two Jokers (cards 53 and 54):
+the chunk of cards above the first Joker swaps places with the chunk
+of cards below the second Joker."
+  (let [joker? (fn [card] (or (= card 53) (= card 54)))
+        [top therest] (split-with #(not (joker? %)) deck)
+        [rbottom rmiddle] (split-with #(not (joker? %)) (reverse therest))]
+    (concat (reverse rbottom) (reverse rmiddle) top)))
--- src/test/clojure/com/timmciver/crypto/test/solitaire.clj
+++ src/test/clojure/com/timmciver/crypto/test/solitaire.clj
@@ -3,6 +3,10 @@
         com.timmciver.crypto.solitaire))
 
 (deftest test-move-card-down
-  (let [orig-deck [5 7 2 4 3 9]
-        mod-deck (move-card-down orig-deck 4 1)]
-    (is (= mod-deck [5 7 2 3 4 9]))))
+  (let [deck [5 7 2 4 3 9]]
+    (is (= (move-card-down deck 4 1) [5 7 2 3 4 9]))
+    (is (= (move-card-down deck 4 4) [5 7 4 2 3 9]))))
+
+(deftest test-triple-cut
+  (let [deck [1 2 3 53 5 6 7 54 8 9]]
+    (is (= (triple-cut deck) [8 9 53 5 6 7 54 1 2 3]))))
