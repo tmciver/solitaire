b73a9923037f10bf85443c9b1cf2228342d9eb20
Tim McIver <tim@timmciver.com>
2012-04-15 11:30:19 -0400

    Updates to project.clj.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject solitaire "0.1.0-SNAPSHOT"
+(defproject solitaire "0.1.0"
   :description "The Solitaire cryptographic algorithm."
+  :url "https://github.com/tmciver/solitaire"
   :dependencies [[org.clojure/clojure "1.2.1"]]
   :source-path "src/main/clojure"
   :test-path "src/test/clojure")
